4e873f2b47b033b2c0028666a63777c061278daa
Eric Kureck <kureck@gmail.com>
2011-11-03 10:28:35 -0200

    limpeza

--- scii/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Scii::Application.config.secret_token = 'c5578d33450122bf47e2b49ae923bcc18818b897f0c17119244ac6f63186d9bc8b1c071db0172198be60309dd39e56173151f954cada0103d1cae6b6d3936bc3'
