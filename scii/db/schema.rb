a53828d1c2f1267e05b4322af381e868440e01e2
Eric Kureck <kureck@gmail.com>
2011-11-21 18:31:59 -0200

    validacao de formulario

--- scii/db/schema.rb
+++ scii/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111121174243) do
+ActiveRecord::Schema.define(:version => 20111121203100) do
 
   create_table "usuarios", :force => true do |t|
     t.string   "nome"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
