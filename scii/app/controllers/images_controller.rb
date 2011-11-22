44c26f9f9187a99e5ccd5d39bcc97e981a44bb7d
Eric Kureck <kureck@gmail.com>
2011-11-22 19:47:27 -0200

    usuario pode editar apenas as imagens que subiu

--- scii/app/controllers/images_controller.rb
+++ scii/app/controllers/images_controller.rb
@@ -1,6 +1,11 @@
 class ImagesController < ApplicationController
 	before_filter :authenticate_usuario!, :except => [:index, :show]
-
+	
+	rescue_from ActiveRecord::RecordNotFound do
+		flash[:notice] = 'The object you tried to access does not exist'
+		redirect_to :action => :index   # or e.g. redirect_to :action => :index
+	end
+	
 	def index
 		@image = Image.all
 	end
@@ -10,7 +15,8 @@ class ImagesController < ApplicationController
 	end
 	
 	def create
-		@image = Image.new(params[:image].merge!(:usuario => current_usuario))
+		#@image = Image.new(params[:image].merge!(:usuario => current_usuario))
+		@image = current_usuario.images.build(params[:image])
 		if	@image.save
 			flash[:notice] = "Imagem criada."
 			redirect_to @image
@@ -21,18 +27,18 @@ class ImagesController < ApplicationController
 	end
 	
 	def edit
-		@image = Image.find(params[:id])
+		@image = current_usuario.images.find(params[:id])
 	end
 	
 	def update
-		@image = Image.find(params[:id])
+		@image = current_usuario.images.find(params[:id])
 		@image.update_attributes(params[:image])
 		flash[:notice] = "Imagem atualizada."
 		redirect_to @image
 	end
 	
 	def destroy
-		@image = Image.find(params[:id])
+		@image = current_usuario.images.find(params[:id])
 		@image.destroy
 		flash[:notice] = "Imagem foi removida"
 		redirect_to images_path
