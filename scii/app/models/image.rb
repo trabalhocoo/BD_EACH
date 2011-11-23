2781b8cc44e9235dcfa96da4f5e69b7404c1c3f6
Eric Kureck <kureck@gmail.com>
2011-11-23 11:34:50 -0200

    criacao de tres tamanhos para imagens

--- scii/app/models/image.rb
+++ scii/app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
 	belongs_to :usuario
 								
-	has_attached_file :photo, :styles => {:small => "150x150>"}
+	has_attached_file :photo, :styles => {:small => "150x150>", :medium => "300x300", :large => "600X600"}
 	
 	validates_attachment_presence :photo
 	validates_attachment_size :photo, :less_than => 5.megabytes
